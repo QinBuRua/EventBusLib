b947ea7cec0cd3436e33b612764b4fbd3f102999
QinBu_Rua <Qmr_1104@163.com>
2026-06-20 21:21:37 +0800

    文档(.gitmessage): 1.新增"移除"类型,更新序号后空格示例,更新多类型格式,添加命名空间省略规则 格式(Core.EventBus): 1.移除ContainsSubscriber方法文档

--- Core/EventBus.cs
+++ Core/EventBus.cs
@@ -25,15 +25,7 @@ public partial class EventBus //todo: 线程安全
         public partial bool TryGetOnHandleExceptions(out List<SubscriberTokenExceptionPair>? onHandleExceptions);
         public partial bool TryGetOnDestroyExceptions(out List<SubscriberTokenExceptionPair>? onDestroyExceptions);
     }
-
-    /// <summary>
-    /// Check whether the subscriber exists in this EventBus.
-    /// </summary>
-    /// <param name="subscriber">the subscriber you want to check, can be null.</param>
-    /// <returns>
-    /// <c>true</c> means it exists.
-    /// <c>false</c> means it does NOT exist, or <paramref name="subscriber"/> is null.
-    /// </returns>
+    
     public partial bool ContainsSubscriber(ISubscriber subscriber);
 
     public partial void PushEvent<TEvent>(TEvent @event)
